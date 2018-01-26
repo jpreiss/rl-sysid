c04b7f8f6a6ea7cdd39bed0374601fc75805ecf2
James Preiss <japreiss@usc.edu>
2018-01-25 18:57:52 -0800

    refactor: namedtuple for dimensions

--- sysid_batch_policy.py
+++ sysid_batch_policy.py
@@ -2,15 +2,18 @@ from baselines.common.mpi_running_mean_std import RunningMeanStd
 import baselines.common.tf_util as U
 import tensorflow as tf
 import gym
-from baselines.common.distributions import make_pdtype
+from baselines.common.distributions import DiagGaussianPdType
 import numpy as np
+from collections import namedtuple
 
-def MLPModule(last_out, n_hid, hid_size, last_initializer, n_out, name):
-        for i in range(n_hid):
-            last_out = tf.nn.tanh(U.dense(last_out, hid_size,
-                name+"fc%i"%(i+1), weight_init=U.normc_initializer(1.0)))
-        return U.dense(last_out, n_out,
-                name+"final", weight_init=U.normc_initializer(last_initializer))
+# ob: observation without sysid added
+# sysid: mass, inertia, etc...
+# ob_concat: ob + sysid
+# ac: action
+# embed: dimensionality of embedding space
+# agents: number of agents in batch environment
+# window: length of rolling window for sysid network input
+Dim = namedtuple('Dim', 'ob sysid ob_concat ac embed agents window')
 
 class SysIDPolicy(object):
     recurrent = False
@@ -19,59 +22,63 @@ class SysIDPolicy(object):
             self._init(*args, **kwargs)
             self.scope = tf.get_variable_scope().name
 
-    def _init(self, ob_space, ac_space, sysid_dim, latent_dim, traj_len, hid_size, n_hid, gaussian_fixed_var=True):
-        assert isinstance(ob_space, gym.spaces.Box)
-        assert isinstance(ac_space, gym.spaces.Box)
-        assert len(ob_space.shape) == 1
-        assert len(ac_space.shape) == 1
-        ob_dim = ob_space.shape[0] - sysid_dim
-        ac_dim = ac_space.shape[0]
+    # set up the network
+    def _init(self, dim, hid_size, n_hid, gaussian_fixed_var=True):
 
-        self.ob_dim = ob_dim
-        self.traj_len = traj_len
-        self.latent_dim = latent_dim
-        self.pdtype = pdtype = make_pdtype(ac_space)
+        def MLPModule(last_out, n_hid, hid_size, last_initializer, n_out, name):
+                for i in range(n_hid):
+                    last_out = tf.nn.tanh(U.dense(last_out, hid_size,
+                        name+"fc%i"%(i+1), weight_init=U.normc_initializer(1.0)))
+                return U.dense(last_out, n_out,
+                        name+"final", weight_init=U.normc_initializer(last_initializer))
+
+        self.dim = dim
         self.alpha_sysid = 0.5
-        sequence_length = None
 
-        self.ob = U.get_placeholder(name="ob", dtype=tf.float32, shape=[sequence_length] + list(ob_space.shape))
-        with tf.variable_scope("obfilter"):
-            self.ob_rms = RunningMeanStd(shape=ob_space.shape)
-        obz = tf.clip_by_value((self.ob - self.ob_rms.mean) / self.ob_rms.std, -5.0, 5.0)
-        obz, sysidz = tf.split(obz, [ob_dim, sysid_dim], axis=1)
+        self.ob = U.get_placeholder(name="ob", dtype=tf.float32, shape=(None, dim.ob_concat))
+        with tf.variable_scope("ob_filter"):
+            self.ob_rms = RunningMeanStd(shape=(dim.ob_concat))
+            obz = tf.clip_by_value(
+                (self.ob - self.ob_rms.mean) / self.ob_rms.std,
+                -5.0, 5.0, name="ob_normalizer")
+            obz, sysidz = tf.split(obz, [dim.ob, dim.sysid], axis=1)
 
-        self.embed = MLPModule(sysidz, n_hid, hid_size, 0.0, latent_dim, "embed")
+        with tf.variable_scope("embed"):
+            self.embed = MLPModule(sysidz, n_hid, hid_size, 0.0, dim.embed, "embed")
 
-        obz_and_embed = tf.concat([obz, self.embed], axis=1)
+        obz_and_embed = tf.concat([obz, self.embed], axis=1, name="input_concat")
 
-        self.vpred = MLPModule(obz_and_embed, n_hid, hid_size, 1.0, 1, "vf")[:,0]
-        #self.vpred = MLPModule(obz, n_hid, hid_size, 1.0, 1, "vf")[:,0]
+        with tf.variable_scope("policy"):
+            mean = MLPModule(obz_and_embed, n_hid, hid_size, 0.01, dim.ac, "pol")
+            logstd = tf.get_variable(name="logstd", shape=[1, dim.ac], 
+                initializer=tf.zeros_initializer())
+            pdparam = U.concatenate([mean, mean * 0.0 + logstd], axis=1)
+            self.pdtype = DiagGaussianPdType(dim.ac)
+            self.pd = self.pdtype.pdfromflat(pdparam)
 
-        mean = MLPModule(obz_and_embed, n_hid, hid_size, 0.01, ac_dim, "pol")
-        logstd = tf.get_variable(name="logstd", shape=[1, ac_dim], 
-            initializer=tf.zeros_initializer())
-        pdparam = U.concatenate([mean, mean * 0.0 + logstd], axis=1)
-        self.pd = pdtype.pdfromflat(pdparam)
+        with tf.variable_scope("value"):
+            self.vpred = MLPModule(obz_and_embed, n_hid, hid_size, 1.0, 1, "vf")[:,0]
 
-        # SysID inputs, network, and loss function
-        self.traj_ob = U.get_placeholder(name="traj_ob",
-            dtype=tf.float32, shape=[None, traj_len, ob_dim])
-        self.traj_ac = U.get_placeholder(name="traj_ac",
-            dtype=tf.float32, shape=[None, traj_len, ac_dim])
-        trajs_flat = tf.layers.flatten(tf.concat(
-            [self.traj_ob, self.traj_ac], axis=2))
-        self.traj2embed = MLPModule(trajs_flat,
-            n_hid, hid_size, 1.0, latent_dim, "traj2embed")
-        self.sysid_err_supervised = tf.losses.mean_squared_error(
-            tf.stop_gradient(self.embed), self.traj2embed)
+        with tf.variable_scope("sysid"):
+            # SysID inputs, network, and loss function
+            self.traj_ob = U.get_placeholder(name="traj_ob",
+                dtype=tf.float32, shape=[None, dim.window, dim.ob])
+            self.traj_ac = U.get_placeholder(name="traj_ac",
+                dtype=tf.float32, shape=[None, dim.window, dim.ac])
+            trajs_flat = tf.layers.flatten(tf.concat(
+                [self.traj_ob, self.traj_ac], axis=2))
+            self.traj2embed = MLPModule(trajs_flat,
+                n_hid, hid_size, 1.0, dim.embed, "traj2embed")
+            self.sysid_err_supervised = tf.losses.mean_squared_error(
+                tf.stop_gradient(self.embed), self.traj2embed)
 
-        self.state_in = []
-        self.state_out = []
+        with tf.variable_scope("stochastic_switch"):
+            self.stochastic = tf.placeholder(dtype=tf.bool, shape=(), name="stochastic")
+            self.ac = U.switch(self.stochastic, self.pd.sample(), self.pd.mode())
 
-        self.stochastic = tf.placeholder(dtype=tf.bool, shape=())
-        self.ac = U.switch(self.stochastic, self.pd.sample(), self.pd.mode())
         self._act = U.function([self.stochastic, self.ob], [self.ac, self.vpred])
 
+    # given the actual dynamics parameters, compute the embedding
     def sysid_to_embedded(self, sysid_vals):
         sz = sysid_vals.shape
         if len(sz) < 2:
@@ -79,7 +86,7 @@ class SysIDPolicy(object):
         else:
             assert len(sz) == 2
         k = sysid_vals.shape[0]
-        sysid_vals = np.concatenate([np.zeros((k, self.ob_dim)), sysid_vals], axis=1)
+        sysid_vals = np.concatenate([np.zeros((k, self.dim.ob)), sysid_vals], axis=1)
         sess = tf.get_default_session()
         embed = sess.run(self.embed, feed_dict={self.ob: sysid_vals})
         if len(sz) < 2:
@@ -87,7 +94,7 @@ class SysIDPolicy(object):
         else:
             return embed
 
-
+    # given the ob/ac windows, estimate the embedding
     def estimate_sysid(self, ob_trajs, ac_trajs):
         feed = {
             self.traj_ob : ob_trajs,
@@ -97,9 +104,13 @@ class SysIDPolicy(object):
         embed = sess.run(self.traj2embed, feed_dict=feed)
         return embed
 
+    # act - ob is concat(ob, sysid)
     def act(self, stochastic, ob):
         ac1, vpred1 =  self._act(stochastic, ob)
         return ac1, vpred1
+
+    # act, but with a given embedding value supplanting
+    # the embedding computed from the true sysid values
     def act_embed(self, stochastic, ob, embed):
         feed = {
             self.ob : ob,
@@ -110,6 +121,7 @@ class SysIDPolicy(object):
         ac = sess.run(self.ac, feed_dict=feed)
         return ac
 
+    # for OpenAI Baselines compatibility
     def get_variables(self):
         return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope)
     def get_trainable_variables(self):
