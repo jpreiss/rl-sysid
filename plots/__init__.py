ee8b4a4d919425968161953c2ae7040d1a9ca75c
jpreiss <jamesalanpreiss@gmail.com>
2018-09-19 00:28:02 -0700

    plots __init__

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,5 +1,7 @@
 from plots.reward_boxplot import reward_boxplot
+from plots.nested_boxplot import nested_boxplot
 from plots.embed_scatter import embed_scatter
 from plots.embed2d_color import embed2d_color
 from plots.learning_curves import learning_curves
 from plots.flat_rew_hist import flat_rew_hist
+from plots.band_plot import band
