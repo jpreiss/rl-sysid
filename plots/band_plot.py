d4a64c06c776839a500be9bb2f46b0330444dd29
jpreiss <jamesalanpreiss@gmail.com>
2018-09-18 22:11:29 -0700

    fix colors in band_plot

--- plots/band_plot.py
+++ plots/band_plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 # plot a "band of lines" in multiple ways
@@ -10,25 +11,30 @@ import matplotlib.pyplot as plt
 # smoothing: width of moving average filter
 # percentile: used in "bound" mode, default = 5% (implies upper limit 95%)
 # sigmas: used in "std" mode, default = 1 (one standard deviation)
-def band(x, ys, color, smoothing=1, percentile=5.0, sigmas=1.0):
+def band(x, ys, mode, label="", color=None, smoothing=1, percentile=5.0, sigmas=1.0):
 
     def moving_average(x):
         kernel = 1.0 / smoothing * np.ones(smoothing)
         return np.convolve(x, kernel, mode="valid")
 
     if mode == "bound":
-        mins = moving_average(np.percentile(r, percentile, axis=-1))
-        maxes = moving_average(np.percentile(r, percentile, axis=-1))
-        ind = np.arange(len(mins)) + boxwidth // 2 + 1
-        plt.fill_between(x, mins, maxes, color=color, alpha=0.1)
-        plt.plot(x, mins, color=color, linewidth=1.0, label=label)
-        plt.plot(x, maxes, color=color, linewidth=1.0)
+        mins = moving_average(np.percentile(ys, percentile, axis=-1))
+        maxes = moving_average(np.percentile(ys, 100.0 - percentile, axis=-1))
+        ind = np.arange(len(mins)) + smoothing // 2 + 1
+        xind = np.array(x)[ind]
+        line = plt.plot(xind, mins, color=color, linewidth=1.0, label=label)[0]
+        if color is None:
+            color = line.get_color()
+        plt.plot(xind, maxes, color=color, linewidth=1.0)
+        plt.fill_between(xind, mins, maxes, color=color, alpha=0.1)
     elif mode == "std":
-        means = moving_average(np.mean(r, axis=-1))
-        stds = moving_average(np.std(r, axis=-1))
-        x = np.arange(len(means)) + boxwidth // 2 + 1
+        means = moving_average(np.mean(ys, axis=-1))
+        stds = moving_average(np.std(ys, axis=-1))
+        x = np.arange(len(means)) + smoothing // 2 + 1
+        line = plt.plot(x, means, color=color, linewidth=2.0, label=label)[0]
+        if color is None:
+            color = line.get_color()
         plt.fill_between(x, means - stds, means + stds, color=color, alpha=0.1)
-        plt.plot(x, means, color=color, linewidth=2.0, label=label)
     else:
-        for run in r.T:
+        for run in ys.T:
             plt.plot(run, color=color)
