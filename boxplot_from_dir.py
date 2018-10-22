ab6419387c2e042dfc7c7003f5e89984c7229caf
jpreiss <jamesalanpreiss@gmail.com>
2018-10-21 18:46:48 -0700

    csv along with boxplot

--- boxplot_from_dir.py
+++ boxplot_from_dir.py
@@ -25,9 +25,6 @@ def main(rootdir, outpath, condense_seed=True, transpose=False):
     if transpose:
         items = reversed(items)
     keys, multivals = zip(*items)
-    if len(keys) < 2 + int(condense_seed):
-        # TODO
-        keys = ("flavor",) + keys
     if len(keys) > 2 + int(condense_seed):
         # TODO: condense excess keys into Cartesian product
         raise NotImplementedError("handle other amounts of keys")
@@ -39,7 +36,6 @@ def main(rootdir, outpath, condense_seed=True, transpose=False):
 
     results = lib.load_test_results(multispec, rootdir)
     assert len(results) == len(specs)
-    import pdb; pdb.set_trace()
     assert type(results[0][1]) == list
     assert type(results[0][1][0]) == dict
     test_rews = [np.array(list(lib.iter_key(segs, "ep_rews"))).flatten() for _, segs in results]
@@ -48,18 +44,21 @@ def main(rootdir, outpath, condense_seed=True, transpose=False):
     fig = plt.figure(figsize=(6,10))
     plt.style.use("seaborn-dark")
 
+
     multivals, train_table = lib.tabulate(specs, train_rews, keys)
     _, test_table = lib.tabulate(specs, test_rews, keys)
+    # TODO why does tabulate add a singleton dimension for (flavor, seed)
 
     # collapse seed axis (TODO: more numpy-ish way?)
     if "seed" in keys and condense_seed:
+        raise NotImplementedError
         iseed = keys.index("seed")
         order = range(iseed) + range(iseed + 1, len(keys)) + [iseed]
         train_table = lib.flatlast(train_table.permute(order))
         test_table = lib.flatlast(test_table.permute(order))
 
-    sh = train_table.shape[:2]
-    assert test_table.shape[:2] == sh
+    sh = train_table.shape[:len(keys)]
+    assert test_table.shape[:len(keys)] == sh
     train_table = train_table.reshape((sh) + (-1,))
     test_table = test_table.reshape((sh) + (-1,))
 
@@ -70,8 +69,41 @@ def main(rootdir, outpath, condense_seed=True, transpose=False):
     }
     multivals = [[f"{namesub.get(k, k)} = {v}" for v in mv] for k, mv in zip(keys, multivals)]
     labels = multivals + [("train", "test")]
+    if len(multivals) == 1:
+        print("prepending dummy dimension")
+        # TODO actually handle this well
+        labels = [("foo", "bar")] + labels
+        keys = ("dummy",) + keys
+        train_table = np.stack([train_table, train_table])
+        test_table = np.stack([test_table, test_table])
     fig = plots.nested_boxplot(labels, train_table, test_table, aspect_ratio=1.2)
     fig.savefig(outpath)
+
+    texpath = os.path.splitext(outpath)[0] + ".csv"
+    columns = {
+        "median" : lambda r: np.median(r.flat),
+        "mean"   : lambda r: np.mean(r.flat),
+        "min"    : lambda r: np.min(r.flat),
+        "max"    : lambda r: np.max(r.flat),
+        "std"    : lambda r: np.std(r.flat),
+    }
+    with open(texpath, "w") as f:
+        cols = it.product(*labels)
+        labxp = labels[-1:] + labels[:-1]
+        f.write("train_test, " + ", ".join(keys + tuple(columns.keys())) + "\n")
+
+        def rec(labels, table):
+            if labels == []:
+                cells = [func(table) for func in columns.values()]
+                cellstrs = [f"{x:.1f}" for x in cells]
+                yield ", ".join(cellstrs) + "\n"
+            else:
+                for lab, tab in zip(labels[0], table):
+                    for row in rec(labels[1:], tab):
+                        yield f"{lab}, " + row
+
+        for row in rec(labxp, [train_table, test_table]):
+            f.write(row)
 
 
 if __name__ == "__main__":
