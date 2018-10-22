8b548fab8e7701b32f3d60a31f4bf39c0802d21a
jpreiss <jamesalanpreiss@gmail.com>
2018-10-21 18:47:27 -0700

    train & test dir instead of JSON path, auto cpu_count

--- test.py
+++ test.py
@@ -7,18 +7,19 @@ import libexperiment as lib
 
 def main():
     parser = argparse.ArgumentParser(description="test rl-sysid policies based on json config.")
-    parser.add_argument("--procs", type=int, default=6)
+    parser.add_argument("--procs", type=int, default=0)
     parser.add_argument("--sysid-samples", type=int, default=4)
-    parser.add_argument("spec", type=str)
+    parser.add_argument("rootdir", type=str)
     args = parser.parse_args()
 
-    rootdir, name = os.path.split(args.spec)
-    assert name.endswith(".json")
-    with open(args.spec) as f:
-        spec = lib.Spec(json.load(f))
+    specpath = os.path.join(args.rootdir, "config.json")
+    with open(specpath) as f:
+        multispec = lib.Spec(json.load(f))
 
-    rootdir = os.path.join(rootdir, "results")
-    lib.test_multispec(spec, rootdir, args.sysid_samples, args.procs)
+    rootdir = os.path.join(args.rootdir, "results")
+    if args.procs == 0:
+        args.procs = os.cpu_count() // 2 - 1
+    lib.test_multispec(multispec, rootdir, args.sysid_samples, args.procs)
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -8,15 +8,14 @@ import libexperiment as lib
 def main():
     parser = argparse.ArgumentParser(description="train rl-sysid policies based on json config.")
     parser.add_argument("--procs", type=int, default=0)
-    parser.add_argument("spec", type=str)
+    parser.add_argument("rootdir", type=str)
     args = parser.parse_args()
 
-    rootdir, name = os.path.split(args.spec)
-    assert name.endswith(".json")
-    with open(args.spec) as f:
+    specpath = os.path.join(args.rootdir, "config.json")
+    with open(specpath) as f:
         spec = lib.Spec(json.load(f))
 
-    rootdir = os.path.join(rootdir, "results")
+    rootdir = os.path.join(args.rootdir, "results")
     if args.procs == 0:
         args.procs = os.cpu_count() // 2 - 1
     lib.train_multispec(spec, rootdir, args.procs)
